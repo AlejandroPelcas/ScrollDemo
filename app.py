968676c08ded2f4399749ac6f5d69695012100cc
AlejandroPelcas <alejandro.pelcas@berkeley.edu>
2025-06-07 12:48:56 -0700

    Added timer - bare site

---
+++ app.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('timer.html')
+    
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
